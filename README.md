10db2bd18aab79a36127030df02b7c403b568f59
David Shepherd <davidshepherd7@gmail.com>
2015-01-21 13:41:45 +0000

    Update documentation

--- README.md
+++ README.md
@@ -9,6 +9,9 @@ M-space can be used to only insert a space (chosen because the behaviour of
 space with this mode is enabled is very similar to `just-one-space`, which
 is ususally bound to M-space).
 
+Some special behaviour is implemented in a few major modes where the
+default fill-paragraph behaviour would affect code as well as comments.
+
 
 Setup
 =====
@@ -18,3 +21,8 @@ Make sure that the file `aggressive-fill-paragraph.el` is loaded, then call
     (aaf-setup-recommended-hooks)
 
 to enable the minor mode in all places where it might be useful.
+Alternatively use
+
+    (add-hook '[major-mode-hook] #'aggressive-fill-paragraph-mode)
+
+to enable aggressive-fill-paragraph-mode only in specified major modes.
--- aggressive-fill-paragraph.el
+++ aggressive-fill-paragraph.el
@@ -1,4 +1,4 @@
-;;; aggressive-fill-paragraph.el --- A minor mode to keep comment paragraphs filled
+;;; aggressive-fill-paragraph.el --- A minor mode to automatically keep comment and prose paragraphs filled
 
 ;; Author: David Shepherd <davidshepherd7@gmail.com>
 ;; Version: 0.0.1
@@ -8,6 +8,16 @@
 
 ;;; Commentary:
 
+;; An emacs minor-mode for keeping paragraphs filled in both comments and prose.
+
+;; Each time a space is inserted the current paragraph is refilled.
+
+;; M-space can be used to only insert a space (chosen because the behaviour
+;; of space with this mode is enabled is very similar to `just-one-space`,
+;; which is ususally bound to M-space).
+
+;; Some special behaviour is implemented in a few major modes where the
+;; default fill-paragraph behaviour would affect code as well as comments.
 
 ;;; Code:
 
